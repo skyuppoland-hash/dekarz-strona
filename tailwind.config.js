9d6604795cb44a52ea37556f931b3a8448023092
skyuppoland-hash <skyuppoland@gmail.com>
2026-01-07 18:09:34 +0100

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
-module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
